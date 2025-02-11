f670a9931b1a88e10af4778f60ebb21020127d3f
pxpm <pxpm88@gmail.com>
2025-02-11 12:46:26 +0000

    wip

---
+++ app/Http/Controllers/Admin/UserCrudController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\UserRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class UserCrudController
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class UserCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     *
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\User::class);
+        CRUD::setRoute(config('backpack.base.route_prefix').'/user');
+        CRUD::setEntityNameStrings('user', 'users');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     *
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+        CRUD::column('name')->type('text')->limit(9999999999999);
+        CRUD::column('name2')->type('text');
+        CRUD::column('name5')->type('text');
+
+        CRUD::column('name4')->type('text');
+
+        CRUD::column('name3')->type('text');
+
+        CRUD::column('name6')->type('text');
+        CRUD::column('name7')->type('text');
+        CRUD::column('name8')->type('text');
+        CRUD::column('name9')->type('text');
+        CRUD::column('name10')->type('text');
+        CRUD::column('name11')->type('text');
+        CRUD::column('name12')->type('text');
+        CRUD::column('name13')->type('text');
+        CRUD::column('name14')->type('text');
+        CRUD::column('name15')->type('text');
+        CRUD::column('name16')->type('text');
+        CRUD::column('name17')->type('text');
+        CRUD::column('name18')->type('text');
+        CRUD::column('name19')->type('text');
+        CRUD::column('name20')->type('text');
+
+        CRUD::enableResponsiveTable();
+
+        /**
+         * Columns can be defined using the fluent syntax:
+         * - CRUD::column('price')->type('number');
+         */
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     *
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(UserRequest::class);
+        CRUD::field('name')->type('text');
+        /**
+         * Fields can be defined using the fluent syntax:
+         * - CRUD::field('price')->type('number');
+         */
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     *
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
